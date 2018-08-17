b7459902b47ffa5a3b66d299f34acc9657cb6505
byPilaf <a492942809@live.com>
2018-08-17 21:56:29 +0800

    赛事表创建

--- byPliaf说明文档.md
+++ byPliaf说明文档.md
@@ -10,14 +10,24 @@
 | username | string(20)       | 用户名                 |
 | password | sting(255)       | 密码                   |
 | name     | string(10)       | 姓名                   |
-| gender   | enum(男,女,保密) | 性别                   |
+| gender   | enum('男','女','保密') | 性别                   |
 | mobile   | char(11)         | 手机号                 |
 | email    | string(40)       | Email                  |
 | status   | enum(1,2)        | 用户状态,1=禁用,2=启用 |
 
 
 
+###数据表(后台赛事表)
 
+####表名:competitions 
+|   字段名  |数据类型    |  字段功能  |
+| ---------|------------| ----------- |
+| id       |                     | 自增主键  |
+| competitions_name            | string(50) | 赛事名称 |
+| date     | date                | 赛事日期  |
+| events   | enum('男单','男双','女单','女双','混双','男团','女团') | 比赛项目  |
+| counrty   | string(20)     | 比赛国家     |
+| city   | string(20)           | 比赛城市  |
 
 
 
--- database/migrations/2018_08_17_200318_create_competitions_table.php
+++ database/migrations/2018_08_17_200318_create_competitions_table.php
@@ -13,7 +13,15 @@ class CreateCompetitionsTable extends Migration
      */
     public function up()
     {
-        //
+        //创建管理员数据表(competitions)
+        Schema::create('competitions',function(Blueprint $table){
+            $table -> increments('id'); //自增主键
+            $table -> string('competitions_name',50) -> notnull() -> comment('赛事名称');
+            $table -> date('date') -> notnull() -> comment('赛事日期');
+            $table -> enum('events',['男单','男双','女单','女双','混双','男团','女团']) -> notnull() -> comment('比赛项目');
+            $table -> string('counrty',20) -> notnull() -> comment('比赛国家');
+            $table -> string('city',20) -> notnull() -> comment('比赛城市');
+        });
     }
 
     /**
@@ -23,6 +31,7 @@ class CreateCompetitionsTable extends Migration
      */
     public function down()
     {
-        //
+        //删除管理员数据表(competitions)
+        Schema::dropIfExists('competitions');
     }
 }
