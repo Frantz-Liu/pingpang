2062f51d6ba80f6a14fd2af111c90a9dfff9a88b
Jeason_Ro <m1156213167@163.com>
2018-08-17 13:49:33 +0800

    修改注释

--- routes/web.php
+++ routes/web.php
@@ -10,21 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 //后台首页路由
 Route::get('admin/index/index', 'Admin\IndexController@index')->name('dashboard');//
 Route::get('admin/index/welcome', 'Admin\IndexController@welcome')->name('welcome');//
-=======
-<<<<<<< Updated upstream
->>>>>>> 1ce61ce2692f2153dcba43350133df9a8fb953cc
->>>>>>> 0fd2e1f411b23e758e4a85805b25177173dba08c
-
-Route::get('/', function () {
-    return view('welcome');
-});
 //运动员管理
 // 人员列表展示
 Route::get('user/index'.'Admin\UserController@index')-> name('user_index');
